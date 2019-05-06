2131586dd45834a451a630beea05f06ace412efc
sveti <svetozara_stm@abv.bg>
2019-05-06 14:48:58 +0300

    register button has been centered

--- src/main/java/com/java/nbu/hotels/HotelsApplication.java
+++ src/main/java/com/java/nbu/hotels/HotelsApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class HotelsApplication {
    public static void main(String[] args) {
